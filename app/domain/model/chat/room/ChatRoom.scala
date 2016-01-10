7c3e6e1788e2df396346f437f5c0e19586a52bcb
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-01-10 20:32:56 +0900

    fix chat room model

--- app/domain/model/chat/room/ChatRoom.scala
+++ app/domain/model/chat/room/ChatRoom.scala
@@ -1,6 +1,7 @@
 package domain.model.chat.room
 
+import domain.model.Entity
 import domain.model.chat.member.MemberId
 import domain.model.chat.message.MessageId
 
-case class ChatRoom(id: ChatRoomId, name: String, participants: Set[MemberId], messages: Seq[MessageId])
+case class ChatRoom(id: ChatRoomId, name: String, participants: Set[MemberId], messages: Seq[MessageId]) extends Entity
