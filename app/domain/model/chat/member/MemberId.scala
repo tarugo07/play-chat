09b207ca3a32d1f9ab1b7c5a0792be7d1cd958f4
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-02 18:03:39 +0900

    add undefined ID type

--- app/domain/model/chat/member/MemberId.scala
+++ app/domain/model/chat/member/MemberId.scala
@@ -2,4 +2,24 @@ package domain.model.chat.member
 
 import domain.model.ValueObject
 
-case class MemberId(value: Long) extends ValueObject
+case class MemberId(value: Long) extends ValueObject {
+
+  def isUndefined: Boolean = {
+    value == MemberId.undefinedValue
+  }
+
+}
+
+case class UndefinedMemberId() {
+
+  def toMemberId: MemberId = {
+    MemberId(value = MemberId.undefinedValue)
+  }
+
+}
+
+object MemberId {
+
+  val undefinedValue = 0
+
+}
--- app/domain/model/chat/room/ChatRoomId.scala
+++ app/domain/model/chat/room/ChatRoomId.scala
@@ -2,4 +2,24 @@ package domain.model.chat.room
 
 import domain.model.ValueObject
 
-case class ChatRoomId(value: Long) extends ValueObject
+case class ChatRoomId(value: Long) extends ValueObject {
+
+  def isUndefined: Boolean = {
+    value == ChatRoomId.undefinedValue
+  }
+
+}
+
+case class UndefinedChatRoomId() {
+
+  def toChatRoomId: ChatRoomId = {
+    ChatRoomId(value = ChatRoomId.undefinedValue)
+  }
+
+}
+
+object ChatRoomId {
+
+  val undefinedValue = 0
+
+}
