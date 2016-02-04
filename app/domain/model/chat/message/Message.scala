5b205f53572e3e5808da6739768885fd9e6e23a4
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-04 13:30:29 +0900

    refactoring

--- app/domain/model/chat/message/Message.scala
+++ app/domain/model/chat/message/Message.scala
@@ -2,8 +2,26 @@ package domain.model.chat.message
 
 import java.time.ZonedDateTime
 
-import domain.model.Entity
 import domain.model.chat.channel.ChannelId
 import domain.model.chat.member.Member
+import domain.model.{Entity, ValueObject}
 
-case class Message(id: MessageId, text: String, channelId: ChannelId, sender: Member, occurredOn: ZonedDateTime) extends Entity
+case class MessageId(value: Long) extends ValueObject {
+  def isUndefined: Boolean = {
+    value == MessageId.undefinedValue
+  }
+}
+
+case class UndefinedMessageId() {
+  def toMessageId: MessageId = MessageId(value = MessageId.undefinedValue)
+}
+
+object MessageId {
+  val undefinedValue = 0
+}
+
+case class MessageContent(content: String) extends ValueObject {
+  require(content.length > 0 && content.length <= 255)
+}
+
+case class Message(id: MessageId, text: MessageContent, channelId: ChannelId, sender: Member, occurredOn: ZonedDateTime) extends Entity
