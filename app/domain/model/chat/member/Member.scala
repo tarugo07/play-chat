878682bf10442efa56d502cb0eac00619c1ce586
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-04 13:35:36 +0900

    refactoring

--- app/domain/model/chat/member/Member.scala
+++ app/domain/model/chat/member/Member.scala
@@ -1,11 +1,27 @@
 package domain.model.chat.member
 
-import domain.model.Entity
+import domain.model.{Entity, ValueObject}
+
+case class MemberId(value: Long) extends ValueObject {
+  def isUndefined: Boolean = {
+    value == MemberId.undefinedValue
+  }
+}
+
+case class UndefinedMemberId() {
+  def toMemberId: MemberId = MemberId(value = MemberId.undefinedValue)
+}
+
+object MemberId {
+  val undefinedValue = 0
+}
+
+case class MemberName(name: String) extends ValueObject {
+  require(name.length > 0 && name.length <= 32)
+}
 
 case class Member(id: MemberId, name: MemberName) extends Entity {
-
   def changeName(name: MemberName): Member = {
     this.copy(name = name)
   }
-
 }
--- app/domain/model/chat/member/MemberName.scala
+++
@@ -1,9 +0,0 @@
-package domain.model.chat.member
-
-import domain.model.ValueObject
-
-case class MemberName(name: String) extends ValueObject {
-
-  require(name.length > 0 && name.length <= 32)
-
-}
