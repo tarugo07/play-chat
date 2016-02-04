ce676ec24f7a1f70c0eabcf0900c091a8f0a0c3f
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-04 13:11:18 +0900

    refactoring

--- app/domain/model/chat/channel/Channel.scala
+++ app/domain/model/chat/channel/Channel.scala
@@ -1,7 +1,25 @@
 package domain.model.chat.channel
 
-import domain.model.Entity
 import domain.model.chat.member.{Member, MemberId}
+import domain.model.{Entity, ValueObject}
+
+case class ChannelId(value: Long) extends ValueObject {
+  def isUndefined: Boolean = {
+    value == ChannelId.undefinedValue
+  }
+}
+
+case class UndefinedChannelId() {
+  def toChannelId: ChannelId = ChannelId(value = ChannelId.undefinedValue)
+}
+
+object ChannelId {
+  val undefinedValue = 0
+}
+
+case class ChannelName(name: String) extends ValueObject {
+  require(name.length > 0 && name.length <= 128)
+}
 
 case class Channel(id: ChannelId, name: ChannelName, participants: Set[MemberId]) extends Entity {
 
--- app/domain/model/chat/channel/ChannelName.scala
+++
@@ -1,9 +0,0 @@
-package domain.model.chat.channel
-
-import domain.model.ValueObject
-
-case class ChannelName(name: String) extends ValueObject {
-
-  require(name.length > 0 && name.length <= 128)
-
-}
