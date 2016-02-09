6f2395c93e56a1a09d25fea7229508d41d0d8afa
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-09 19:40:01 +0900

    add account repository method

--- app/domain/model/account/AccountRepository.scala
+++ app/domain/model/account/AccountRepository.scala
@@ -4,6 +4,12 @@ import scala.util.Try
 
 trait AccountRepository {
 
+  def nextIdentity(): AccountId
+
   def accountOfIdentity(id: AccountId): Try[Account]
-  
+
+  def accountOfMailAddress(mailAddress: MailAddress): Try[Account]
+
+  def save(account: Account): Try[Account]
+
 }
