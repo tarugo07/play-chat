41ff2bba0d9c8d3bff00f653e2f59f1b61023d59
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-12 18:51:08 +0900

    fix evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,17 @@
 # --- !Ups
 
-CREATE TABLE account (
-    id BIGINT(20) NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    mail_adress VARCHAR(256) NOT NULL,
-    create_time TIMESTAMP NOT NULL,
-    update_time TIMESTAMP NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (mail_adress)
-);
+CREATE TABLE IF NOT EXISTS `play_chat`.`account` (
+    `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(128) NOT NULL,
+    `mail_adress` VARCHAR(256) NOT NULL,
+    `create_time` TIMESTAMP NOT NULL,
+    `update_time` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4;
+
 
 # --- !Downs
 
