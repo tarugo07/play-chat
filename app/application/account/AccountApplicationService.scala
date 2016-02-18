f64b164b6bda690bcde4e63693c81783a1911631
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-02-18 13:49:42 +0900

    Add sign up command

--- app/application/account/AccountApplicationService.scala
+++ app/application/account/AccountApplicationService.scala
@@ -5,13 +5,15 @@ import java.security.MessageDigest
 import domain.model.EntityNotFoundException
 import domain.model.account._
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
-case class RegisterAccountCommand(name: String, password: String, mail: String)
+case class SignInAccountCommand(name: String, password: String, mail: String)
+
+case class SignUpAccountCommand(mail: String, password: String)
 
 class AccountApplicationService(accountRepository: AccountRepository) {
 
-  def registerAccount(command: RegisterAccountCommand): Try[Account] = {
+  def signIn(command: SignInAccountCommand): Try[Account] = {
     // TODO: ハッシュ化見直し
     val password = MessageDigest.getInstance("SHA-512")
       .digest(command.password.getBytes).map("%02x".format(_)).mkString
@@ -24,10 +26,24 @@ class AccountApplicationService(accountRepository: AccountRepository) {
     )
 
     accountRepository.accountOfMail(account.mail).map { account =>
-      throw new Exception(s"this mail address that has already been registered: id = ${account.id.value}, mail = ${account.mail.value}")
+      throw new Exception(s"this mail has already been registered: mail = ${account.mail.value}")
     } recoverWith {
-      case ex: EntityNotFoundException =>
-        accountRepository.save(account)
+      case ex: EntityNotFoundException => accountRepository.save(account)
+    }
+  }
+
+  def signUp(command: SignUpAccountCommand): Try[Account] = Try {
+    // TODO: ハッシュ化見直し
+    val password = MessageDigest.getInstance("SHA-512")
+      .digest(command.password.getBytes).map("%02x".format(_)).mkString
+
+    accountRepository.accountOfMail(AccountMail(command.mail)) match {
+      case Success(account) =>
+        if (account.password != AccountPassword(password)) {
+          throw new Exception(s"authentication failure: mail = ${command.mail}")
+        } else
+          account
+      case Failure(ex) => throw ex
     }
   }
 
