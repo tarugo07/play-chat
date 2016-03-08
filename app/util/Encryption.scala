87e2e4dc7faa47f9d428747c8bb0f6f4717eb129
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-03-08 21:01:10 +0900

    fix Ecnryption

--- app/util/Encryption.scala
+++ app/util/Encryption.scala
@@ -4,18 +4,16 @@ import java.security.InvalidKeyException
 import javax.crypto.Cipher
 import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
 
-import org.apache.commons.codec.binary.Base64
-
 import scala.util.{Failure, Try}
 
 object Encryption {
 
-  def encrypt(value: String, algorithm: String, secretKey: String, initVector: String, blockMode: String, padding: String): Try[String] = {
+  def encrypt(value: Array[Byte], algorithm: String, secretKey: String, initVector: String, blockMode: String, padding: String): Try[Array[Byte]] = {
     Try {
       val secretKeySpec = new SecretKeySpec(secretKey.getBytes("UTF-8"), algorithm)
       val cipher = Cipher.getInstance(algorithm, s"/$blockMode/$padding")
       cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(initVector.getBytes("UTF-8")))
-      Base64.encodeBase64String(cipher.doFinal(value.getBytes("UTF-8")))
+      cipher.doFinal(value)
     } recoverWith {
       case ex: InvalidKeyException => Failure(ex)
     }
