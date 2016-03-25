16223c98f4e69ee2ae66a620df759579b4543598
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-03-25 18:39:57 +0900

    add to change account name api

--- app/port/adapter/api/controllers/AccountController.scala
+++ app/port/adapter/api/controllers/AccountController.scala
@@ -1,6 +1,6 @@
 package port.adapter.api.controllers
 
-import application.account.{AccountApplicationService, ChangeAccountPasswordCommand}
+import application.account.{ChangeAccountNameCommand, AccountApplicationService, ChangeAccountPasswordCommand}
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import play.api.mvc._
@@ -9,10 +9,12 @@ import scala.util.{Failure, Success}
 
 class AccountController extends Controller with ControllerSupport {
 
-  implicit val reads: Reads[(String, String)] = (
+  implicit val changePasswordReads: Reads[(String, String)] = (
     (__ \ "currentPassword").read[String] and
       (__ \ "newPassword").read[String]
     ).tupled
+
+  implicit val changeNameReads: Reads[(String)] = (__ \ "name").read[String]
 
   val accountApplicationService = new AccountApplicationService(accountRepository)
 
@@ -21,14 +23,22 @@ class AccountController extends Controller with ControllerSupport {
       case (currentPassword, newPassword) =>
         val command = ChangeAccountPasswordCommand(authenticated.account.id.value, currentPassword, newPassword)
         accountApplicationService.changePassword(command) match {
-          case Success(accessToken) =>
-            Ok(Json.obj("result" -> "OK"))
-          case Failure(ex) =>
-            BadRequest(Json.obj("result" -> "NG"))
+          case Success(_) => Ok(Json.obj("result" -> "OK"))
+          case Failure(ex) => BadRequest(Json.obj("result" -> "NG"))
         }
-    }.recoverTotal { e =>
-      BadRequest(Json.obj("status" -> "NG"))
-    }
+    }.recoverTotal { _ => BadRequest(Json.obj("status" -> "NG")) }
+  }
+  
+  def changeName = AuthAction(parse.json) { implicit authenticated =>
+    authenticated.request.body.validate[(String)].map {
+      case (name) =>
+        val command = ChangeAccountNameCommand(authenticated.account.id.value, name)
+        accountApplicationService.changeName(command) match {
+          case Success(_) => Ok(Json.obj("result" -> "OK"))
+          case Failure(ex) => InternalServerError(Json.obj("result" -> "NG"))
+        }
+    }.recoverTotal { _ => BadRequest(Json.obj("status" -> "NG")) }
+    
   }
 
 }
