97b1c443c4b786ebb143e78e933888eff313a622
Tomoyoshi Ogura <tomoyoshi.ogura@gmail.com>
2016-03-28 11:30:25 +0900

    fix bug

--- app/port/adapter/api/controllers/AccountController.scala
+++ app/port/adapter/api/controllers/AccountController.scala
@@ -1,52 +1,50 @@
 package port.adapter.api.controllers
 
-import application.account.{ChangeAccountNameCommand, AccountApplicationService, ChangeAccountPasswordCommand}
+import application.account.{AccountApplicationService, ChangeAccountNameCommand, ChangeAccountPasswordCommand}
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import play.api.mvc._
 
 import scala.util.{Failure, Success}
 
+case class ChangePassword(currentPassword: String, newPassword: String)
+
+case class ChangeName(name: String)
+
 class AccountController extends Controller with ControllerSupport {
 
-  implicit val changePasswordReads: Reads[(String, String)] = (
+  implicit val changePasswordReads: Reads[ChangePassword] = (
     (__ \ "currentPassword").read[String] and
-      (__ \ "newPassword").read[String]
-    ).tupled
+    (__ \ "newPassword").read[String]
+  )(ChangePassword.apply _)
 
-  implicit val changeNameReads: Reads[String] = (__ \ "name").read[String]
+  implicit val changeNameReads: Reads[ChangeName] = (__ \ "name").read[String].map(ChangeName)
 
   val accountApplicationService = new AccountApplicationService(accountRepository)
 
   def changePassword = AuthAction(parse.json) { implicit authenticated =>
-    authenticated.request.body.validate[(String, String)].map {
-      case (currentPassword, newPassword) =>
-        val command = ChangeAccountPasswordCommand(authenticated.account.id.value, currentPassword, newPassword)
+    authenticated.request.body.validate[ChangePassword].map {
+      case changePassword =>
+        val command = ChangeAccountPasswordCommand(
+          authenticated.account.id.value, changePassword.currentPassword, changePassword.newPassword
+        )
         accountApplicationService.changePassword(command) match {
           case Success(_) => Ok(Json.obj("result" -> "OK"))
           case Failure(ex) =>
-            println(ex.getMessage)
             BadRequest(Json.obj("result" -> "NG"))
         }
-    }.recoverTotal { ex =>
-      println(ex)
-      BadRequest(Json.obj("status" -> "NG"))
-    }
+    }.recoverTotal { ex => BadRequest(Json.obj("status" -> "NG")) }
   }
-  
+
   def changeName = AuthAction(parse.json) { implicit authenticated =>
-    println(authenticated.account)
-    println(authenticated.request)
-    val a = authenticated.request.body.validate[String].map {
-      case name: String =>
-        val command = ChangeAccountNameCommand(authenticated.account.id.value, name)
+    authenticated.request.body.validate[ChangeName].map {
+      case changeName =>
+        val command = ChangeAccountNameCommand(authenticated.account.id.value, changeName.name)
         accountApplicationService.changeName(command) match {
           case Success(_) => Ok(Json.obj("result" -> "OK"))
           case Failure(ex) => InternalServerError(Json.obj("result" -> "NG"))
         }
     }.recoverTotal { _ => BadRequest(Json.obj("status" -> "NG")) }
-    println(a)
-    a
   }
 
 }
